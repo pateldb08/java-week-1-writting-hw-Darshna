883203cf25ff94cf8c2a18a3b4f4bbad99c2eb97
pateldb08 <pateldb08@gmail.com>
2023-03-16 17:41:43 +0000

    java-week-1-writting-hw-Darshna

---
+++ src/homework_week_5/Programme_1_BioData.java
@@ -0,0 +1,24 @@
+package homework_week_5;
+
+/**
+ * 1. Write a programme that prints your BioData.
+ */
+ public class Programme_1_BioData {
+    public static void main(String[] args) {
+        System.out.println("            BIO DATA          ");
+        System.out.println("Name            : Darshna Patel");
+        System.out.println("Mobile          : 0792223338  ");
+        System.out.println("Email           : abc@xyz.com ");
+        System.out.println("Gender          : Female        ");
+        System.out.println("Date of Birth   : 28.02.1996 ");
+        System.out.println("Marital Status  : Single      ");
+        System.out.println("Languages known : English,Spanish ,Gujarati");
+        System.out.println("Qualification   : Software Tester");
+        System.out.println("Experience      : Agile Tester wiht 5 years of experience");
+        System.out.println("Address         : 123 Abc Street, London AB1 2CD         ");
+        System.out.println("Place           : London                                 ");
+        System.out.println("Date            : 02.10.2021                             ");
+        System.out.println("Signature       : DPatel                                 ");
+    }
+}
+
